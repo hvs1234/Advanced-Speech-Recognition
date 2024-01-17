086fa25d66128db7e153d8e4f2c82add4092e675
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2024-01-17 13:05:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,59 @@ createwindow = ()=>{
     newapp.loadFile('index.html')
 }
 app.whenReady().then(createwindow)
+
+/*
+const {app,BrowserWindow} = require('electron')
+
+createwindow = ()=>{
+    const newapp = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {nodeIntegration: true},
+        resizable: false,
+        fullscreenable: false,
+    })
+    newapp.loadFile('index.html')
+}
+app.whenReady().then(createwindow)
+const {app,BrowserWindow} = require('electron')
+
+createwindow = ()=>{
+    const newapp = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {nodeIntegration: true},
+        resizable: false,
+        fullscreenable: false,
+    })
+    newapp.loadFile('index.html')
+}
+app.whenReady().then(createwindow)
+const {app,BrowserWindow} = require('electron')
+
+createwindow = ()=>{
+    const newapp = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {nodeIntegration: true},
+        resizable: false,
+        fullscreenable: false,
+    })
+    newapp.loadFile('index.html')
+}
+app.whenReady().then(createwindow)
+const {app,BrowserWindow} = require('electron')
+
+createwindow = ()=>{
+    const newapp = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {nodeIntegration: true},
+        resizable: false,
+        fullscreenable: false,
+    })
+    newapp.loadFile('index.html')
+}
+app.whenReady().then(createwindow)
+
+*/
